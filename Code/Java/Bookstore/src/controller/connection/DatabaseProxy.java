b161c4166e7cb0cbb7ad12115f6009ab8accaed3
Michi <11michi11@gmail.com>
2018-11-04 19:33:21 +0100

    Weird commit

--- Code/Java/Bookstore/src/controller/connection/DatabaseProxy.java
+++ Code/Java/Bookstore/src/controller/connection/DatabaseProxy.java
@@ -1,9 +1,11 @@
-package Controller.Connection;
+package controller.connection;
 
-import Model.Category;
+import model.Book;
+
+import java.util.List;
 
 public interface DatabaseProxy {
-	public String search(String searchTerm);
-	public String advancedSearch(String title, String author, int year, String isbn, Category category);
+	public List<Book> search(String searchTerm) throws DatabaseConnection.ServerOfflineException, DatabaseConnection.SearchException;
+	public List<Book> advancedSearch(String title, String author, int year, String isbn, Book.Category category) throws DatabaseConnection.ServerOfflineException, DatabaseConnection.SearchException;
 
 }
