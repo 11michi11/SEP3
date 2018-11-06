8229cf27baa72099c232291078a75aa61e323ca3
Michi <11michi11@gmail.com>
2018-11-06 22:00:05 +0100

    BookService

---
+++ Code/Java/BookService/src/main/java/controller/Controller.java
@@ -0,0 +1,43 @@
+package controller;
+
+import com.google.gson.Gson;
+import controller.connection.DatabaseConnection;
+import controller.connection.DatabaseProxy;
+import controller.connection.MockDatabase;
+import model.Book;
+
+import java.util.List;
+
+public class Controller {
+
+    private static Controller instance;
+    private DatabaseProxy db;
+    private Gson gson = new Gson();
+
+    Controller() {
+        this.db = new MockDatabase();
+    }
+
+    public static Controller getInstance() {
+        if (instance == null)
+            instance = new Controller();
+        return instance;
+    }
+
+    public List<Book> search(String searchTerm) throws DatabaseConnection.ServerOfflineException, DatabaseConnection.SearchException {
+        return db.search(searchTerm);
+    }
+
+    public List<Book> advancedSearch(String title, String author, int year, String isbn, Book.Category category) throws DatabaseConnection.ServerOfflineException, DatabaseConnection.SearchException {
+        return db.advancedSearch(title, author, year, isbn, category);
+    }
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        try {
+            System.out.println(controller.search("Tolkien"));
+        } catch (DatabaseConnection.ServerOfflineException | DatabaseConnection.SearchException e) {
+            e.printStackTrace();
+        }
+    }
+}
